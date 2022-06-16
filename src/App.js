a299a3ce6cf4cb6e529c42e0f24cdf24c3a6b0b4
arieelarg <ariel.silva@pedidosya.com>
2022-06-16 19:27:45 -0300

    feat(window): check if wallet is connected with ethereum present in window

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h1>Hello App</h1>;
-}
+import React, { useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window || {};
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("Ethereum object", ethereum);
+    }
+  };
+
+  useEffect(() => checkIfWalletIsConnected(), []);
+
+  return <h1>Hello</h1>;
+};
 
 export default App;
