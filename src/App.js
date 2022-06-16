f9427af6788511ad3812e0c897449ac1b5c92a0b
arieelarg <ariel.silva@pedidosya.com>
2022-06-16 19:51:34 -0300

    fwallet): create button to connect wallet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const checkIfWalletIsConnected = () => {
-    const { ethereum } = window || {};
+const ACCOUNTS = "eth_accounts";
+const REQUEST_ACCOUNTS = "eth_requestAccounts";
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("Ethereum object", ethereum);
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const { ethereum } = window || {};
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("Ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: ACCOUNTS });
+
+      if (!!accounts.length) {
+        const [account] = accounts; // If the user have multiple accounts in their wallet we grab the first one.
+
+        console.log("Found an authorized account:", account);
+
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: REQUEST_ACCOUNTS,
+      });
+
+      console.log("Connected", [accounts]);
+      setCurrentAccount([accounts]);
+    } catch (error) {
+      console.log(error);
     }
   };
 
   useEffect(() => checkIfWalletIsConnected(), []);
 
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello, {currentAccount}</h1>
+      <br />
+      !currentAccount && (
+      <button className="waveButton" onClick={connectWallet}>
+        Connect Wallet
+      </button>
+      )
+    </>
+  );
 };
 
 export default App;
