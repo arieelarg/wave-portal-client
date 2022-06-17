e85148ae9450ddf21d71e7b18a407761c981093e
arieelarg <ariel.silva@pedidosya.com>
2022-06-16 21:07:04 -0300

    feat(wave): implement wave function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 
 const ACCOUNTS = "eth_accounts";
 const REQUEST_ACCOUNTS = "eth_requestAccounts";
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
-
   const { ethereum } = window || {};
+  const contractAddress = "0x3bD605b1d3D6bDDCc06BA4c6c5204249bF1e1355";
+  const contractABI = abi.abi;
+
+  const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
     try {
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("Ethereum object", ethereum);
-      }
+      if (!ethereum) return;
 
       const accounts = await ethereum.request({ method: ACCOUNTS });
 
       if (!!accounts.length) {
         const [account] = accounts; // If the user have multiple accounts in their wallet we grab the first one.
 
-        console.log("Found an authorized account:", account);
-
         setCurrentAccount(account);
       } else {
-        console.log("No authorized account found");
+        // console.log("No authorized account found");
       }
     } catch (err) {
       console.log(err);
@@ -47,6 +44,37 @@ const App = () => {
 
       console.log("Connected", [accounts]);
       setCurrentAccount([accounts]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const wave = async () => {
+    try {
+      if (!ethereum) return;
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+
+      const signer = provider.getSigner();
+
+      const wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      let count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved initial total wave count...", count.toNumber());
+
+      const waveTxn = await wavePortalContract.wave();
+      console.log("Mining...", waveTxn.hash);
+
+      await waveTxn.wait();
+      console.log("Mined -- ", waveTxn.hash);
+
+      count = await wavePortalContract.getTotalWaves();
+
+      console.log("Retrieved modified total wave count...", count.toNumber());
     } catch (error) {
       console.log(error);
     }
@@ -60,9 +88,13 @@ const App = () => {
     <>
       <h1>Hello, {currentAccount}</h1>
       <br />
-      {!currentAccount && (
-        <button className="waveButton" onClick={connectWallet}>
-          Connect Wallet
+      {!currentAccount ? (
+        <button className="button" onClick={connectWallet}>
+          Connect to Wallet
+        </button>
+      ) : (
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
         </button>
       )}
     </>
